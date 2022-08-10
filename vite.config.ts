44d0ac2630dfb509d6d835b44941d42a6c25d69c
elmaester <olegmorket@gmail.com>
2022-08-10 15:02:59 +0300

    add manual chunks

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      manualChunks: {
+        dateTime: ["react-datetime-picker"],
+        fontAwesome: [
+          "@fortawesome/fontawesome-svg-core",
+          "@fortawesome/free-solid-svg-icons",
+          "@fortawesome/react-fontawesome",
+        ],
+        parse: ["parse"],
+      },
+    },
+  },
   plugins: [react()],
 });
