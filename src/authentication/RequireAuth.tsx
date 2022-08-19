b13ec7c84c06eee94b288ff96b53c80ced458023
elmaester <olegmorket@gmail.com>
2022-08-19 16:30:04 +0300

    remove unnecessary import

--- src/authentication/RequireAuth.tsx
+++ src/authentication/RequireAuth.tsx
@@ -1,8 +1,7 @@
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import userIsLoggedIn from "./functions/userIsLoggedIn";
 
 function RequireAuth({ children }: { children: JSX.Element }) {
-  const location = useLocation();
   if (!userIsLoggedIn()) {
     return <Navigate to="/login" replace />;
   }
